52859c637699a7aa52e559e67ae93255c51e362f
o.golikov <o.golikov@dsec.ru>
2024-05-20 17:19:44 +0300

    init step

--- magic_ball.py
+++ magic_ball.py
@@ -1,2 +1,6 @@
-name = input("Enter your name:\n")
-print(f"Welcome, {name}")
+# initial state
+# author @ogolikov
+# Stepik course for beginners
+
+if __name__ == '__main__':
+    print('Magic ball')
